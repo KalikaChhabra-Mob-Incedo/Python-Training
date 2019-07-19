99efc78ff7e1e8559a68600809a75a02a1d26b78
Vivek Srivastava <developer.viveksrivastava@gmail.com>
2019-07-19 18:50:15 +0530

    Create input_output.py

---
+++ Day1/input_output.py
@@ -0,0 +1,11 @@
+# Python program showing  
+# a use of raw_input() 
+  
+g = raw_input("Enter your name : ") 
+print g 
+
+# Python program showing  
+# a use of input() 
+  
+val = input("Enter your value: ") 
+print(val) 
