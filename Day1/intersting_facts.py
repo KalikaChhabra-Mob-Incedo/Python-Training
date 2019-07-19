44646ce1c599073acdb189d2ea47cc3dea4f234f
Vivek Srivastava <developer.viveksrivastava@gmail.com>
2019-07-19 15:08:43 +0530

    Update intersting_facts.py

--- Day1/intersting_facts.py
+++ Day1/intersting_facts.py
@@ -26,3 +26,21 @@ func(a)
 print("2nd Case:") 
 a = [1] 
 func(a) 
+
+
+# Well, python does not have pointers, but the objects are passed to functions by reference (and so in java, another language without pointers). 
+# This mechanism is very similar to passing pointers by value in C.
+
+# We call them references. They work like this
+
+i = 5     # create int(5) instance, bind it to i
+j = i     # bind j to the same int as i
+j = 3     # create int(3) instance, bind it to j
+print i   # i still bound to the int(5), j bound to the int(3)
+
+#Small ints are interned, but that isn't important to this explanation
+
+i = [1,2,3]   # create the list instance, and bind it to i
+j = i         # bind j to the same list as i
+i[0] = 5      # change the first item of i
+print j       # j is still bound to the same list as i
