3b5e9a612dc38f8f4825fb2b587fa60eb8219a81
Vivek Srivastava <developer.viveksrivastava@gmail.com>
2019-07-22 19:43:51 +0530

    Create strings.py

---
+++ Day2/strings.py
@@ -0,0 +1,57 @@
+# all of the following are equivalent
+my_string = 'Hello'
+print(my_string)
+
+my_string = "Hello"
+print(my_string)
+
+my_string = '''Hello'''
+print(my_string)
+
+# triple quotes string can extend multiple lines
+my_string = """Hello, welcome to
+           the world of Python"""
+print(my_string)
+
+
+str = 'programiz'
+print('str = ', str)
+
+#first character
+print('str[0] = ', str[0])
+
+#last character
+print('str[-1] = ', str[-1])
+
+#slicing 2nd to 5th character
+print('str[1:5] = ', str[1:5])
+
+#slicing 6th to 2nd last character
+print('str[5:-2] = ', str[5:-2])
+
+# string concatination
+
+str1 = 'Hello'
+str2 ='World!'
+
+# using +
+print('str1 + str2 = ', str1 + str2)
+
+# using *
+print('str1 * 3 =', str1 * 3)
+
+
+a = 'Hello ' 'Python'
+print(a)
+
+s = ('Hello '
+     'World')
+     
+print(s)
+
+# Iterating through a string using for loop
+count = 0
+for letter in 'Hello World':
+    if(letter == 'l'):
+        count += 1
+print(count,'letters found')
