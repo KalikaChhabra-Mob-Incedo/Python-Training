70732ea2ea872e498d82d1d8e47ccc05cb11b6e2
Vivek Srivastava <developer.viveksrivastava@gmail.com>
2019-07-22 20:10:18 +0530

    Update strings.py

--- Day2/strings.py
+++ Day2/strings.py
@@ -55,3 +55,34 @@ for letter in 'Hello World':
     if(letter == 'l'):
         count += 1
 print(count,'letters found')
+
+
+print("B" in "ABCSCSCS")
+
+str = 'cold'
+
+# enumerate()
+list_enumerate = list(enumerate(str))
+print('list(enumerate(str) = ', list_enumerate)
+
+for i,m in list_enumerate:
+    print(i)
+    
+#character count
+print('len(str) = ', len(str))
+
+
+# default(implicit) order
+default_order = "{}, {} and {}".format('John','Bill','Sean')
+print('\n--- Default Order ---')
+print(default_order)
+
+# order using positional argument
+positional_order = "{1}, {0} and {2}".format('John','Bill','Sean')
+print('\n--- Positional Order ---')
+print(positional_order)
+
+# order using keyword argument
+keyword_order = "{s}, {b} and {j}".format(j='John',b='Bill',s='Sean')
+print('\n--- Keyword Order ---')
+print(keyword_order)
